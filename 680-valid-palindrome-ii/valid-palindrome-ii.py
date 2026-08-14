56cbddce4cad7de29a678cdb6c51e5a3df74d5ee
SUNKARI ROHITH <rohithsunkari123@gmail.com>
2026-08-14 19:16:08 +0530

    Time: 42 ms (88.96%) | Memory: 12.8 MB (8.12%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -2,13 +2,18 @@ class Solution(object):
     def validPalindrome(self, s):
         l=0
         r=len(s)-1
-        while l<r:
+        while l < r:
             if s[l]!=s[r]:
-                skpl,skpr=s[l+1:r+1],s[l:r]
-                return skpl==skpl[::-1] or skpr==skpr[::-1]
+                sl=s[l+1:r+1]
+                sr=s[l:r]
+                return sl == sl[::-1] or sr==sr[::-1]
             l+=1
             r-=1
         return True
+            
+
+
+       
             
 
         
