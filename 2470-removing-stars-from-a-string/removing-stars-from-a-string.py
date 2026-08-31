f165deaf6f7ed93ca7d13dd5f76986c4ebc43122
SUNKARI ROHITH <rohithsunkari123@gmail.com>
2026-08-31 20:33:09 +0530

    Time: 283 ms (42.83%) | Memory: 14.1 MB (30.30%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeStars(self, s):
+        stack=[]
+        for i in s:
+            if stack and i == "*":
+                stack.pop()
+            else:
+                stack.append(i)
+        return "".join(stack)
+        """
+        :type s: str
+        :rtype: str
+        """
+        
